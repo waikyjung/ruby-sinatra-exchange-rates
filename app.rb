f8cba30d65c4df6a579ac8d3827990f93dd21eeb
waikyjung <waiky.jung@gmail.com>
2024-10-17 19:24:58 +0000

    working, but grade doesn't work

--- app.rb
+++ app.rb
@@ -1,9 +1,35 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  currency_list_http = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  raw_response = HTTP.get(currency_list_http)
+  raw_response_parsed = JSON.parse(raw_response).fetch("currencies")
+  @currency_list = []
+  raw_response_parsed.each { |currency| @currency_list.push(currency[0]) }
+  erb(:currency_pairs)
+end
+
+get("/:currency_from") do
+  @currency_from = params.fetch("currency_from")
+
+  currency_list_http = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  raw_response = HTTP.get(currency_list_http)
+  raw_response_parsed = JSON.parse(raw_response).fetch("currencies")
+  @currency_list = []
+  raw_response_parsed.each { |currency| @currency_list.push(currency[0]) }  
+  erb(:convert)
+end
+
+get("/:currency_from/:currency_to") do
+  @currency_from = params.fetch("currency_from")
+  @currency_to = params.fetch("currency_to")
+
+  currency_convert_http = "https://api.exchangerate.host/convert?from=#{@currency_from}&to=#{@currency_to}&amount=1&access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  raw_response = HTTP.get(currency_convert_http)
+  @converted = JSON.parse(raw_response).fetch("result").to_f
+  erb(:result)
 end
